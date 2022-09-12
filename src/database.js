41f38fa4dd128bc26ed2e14b7536ff9c4eec84cd
N-LEE-94 <nathanlee42@btinternet.com>
2022-09-12 12:16:04 +0100

    Bungo has been represented and his name will ring out through the ages

--- src/database.js
+++ src/database.js
@@ -17,28 +17,52 @@ async function main() {
     console.log(greeting);
   };
 
+  function empowerTheVoiceless(primate) {
+    const greeting = primate.name ? "Me name " + primate.name : "I don't have a name";
+    console.log(greeting);
+  }
+
   const Primate = mongoose.model("Primate", primateSchema);
 
-  //const maurice = new Primate({ name: "Maurice" });
-  //const cahaya = new Primate({ name: "Cahaya" });
-  //const nameless = new Primate({});
+  const maurice = new Primate({ name: "Maurice" });
+  const cahaya = new Primate({ name: "Cahaya" });
+  const gort = new Primate({ name: "Gort" });
+  const scrongle = new Primate({ name: "Scrongle" });
+  const jethro = new Primate({ name: "Jethro" });
+  const linus = new Primate({ name: "Linus" });
+  const nameless = new Primate({});
 
-  //await maurice.save();
-  //await cahaya.save();
-  //await nameless.save();
+  // await maurice.save();
+  // await cahaya.save();
+  // await gort.save();
+  // await scrongle.save();
+  // await jethro.save();
+  // await linus.save();
+  // await nameless.save();
 
-  //maurice.speak();
-  //cahaya.speak();
-  //nameless.speak();
+  maurice.speak();
+  cahaya.speak();
+  nameless.speak();
 
-  const primates = await Primate.find();
+  // Primate.updateOne({ name: null }, { name: 'Bungo' }, function(err, res) {
+  //   // Updated at most one doc, `res.nModified` contains the number
+  //   // of docs that MongoDB updated
+  //   if (err) return handleError(err);
+  // });
 
-  Primate.deleteMany({ name: null }, function (err) {
-    if (err) return handleError(err);
-    // deleted at most one tank document
-  });
+  const bungo = await Primate.find({name: "Bungo"});
+  console.log(bungo)
+  // await bungo.save();
+  empowerTheVoiceless(bungo[0]);
+  
+  bungo[0].speak();
 
-  console.log(primates);
+  // Primate.deleteMany({ name: null }, function (err) {
+  //   if (err) return handleError(err);
+  //   // deleted at most one tank document
+  // });
+
+  console.log(bungo);
 
   mongoose.connection.close();
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
