708080371bd6bc24437c8d11645ddb206d42c9da
madisonlowe <mcourteney@live.com>
2022-09-12 11:20:34 +0100

    Deleted primates.
    Co-authored-by: Nathan Lee <100144036+N-LEE-94@users.noreply.github.com>
    Co-authored-by: Fiona Kitchen <100845392+fkit00@users.noreply.github.com>
    Co-authored-by: Andy Johnson <103585624+multi-vit@users.noreply.github.com>

--- src/database.js
+++ src/database.js
@@ -1,38 +1,45 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+let connectionString = process.env.URL;
 
-let connectionString = process.env.URL
-
-
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(connectionString);
   const primateSchema = new mongoose.Schema({
-    name: String
+    name: String,
   });
 
-  
   primateSchema.methods.speak = function speak() {
-    const greeting = this.name
-      ? "Me name " + this.name
-      : "I don't have a name";
+    const greeting = this.name ? "Me name " + this.name : "I don't have a name";
     console.log(greeting);
   };
 
+  const Primate = mongoose.model("Primate", primateSchema);
 
-  const Primate = mongoose.model('Primate', primateSchema);
-  const maurice = new Primate({ name: 'Maurice' });
-  console.log(maurice.name);
-  await maurice.save();
-  maurice.speak() 
+  //const maurice = new Primate({ name: "Maurice" });
+  //const cahaya = new Primate({ name: "Cahaya" });
+  //const nameless = new Primate({});
 
-const primates = await Primate.find();
-console.log(primates);
+  //await maurice.save();
+  //await cahaya.save();
+  //await nameless.save();
 
-mongoose.connection.close()
+  //maurice.speak();
+  //cahaya.speak();
+  //nameless.speak();
 
+  const primates = await Primate.find();
+
+  Primate.deleteMany({ name: null }, function (err) {
+    if (err) return handleError(err);
+    // deleted at most one tank document
+  });
+
+  console.log(primates);
+
+  mongoose.connection.close();
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 }
